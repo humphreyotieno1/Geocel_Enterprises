2c366c102aade2528254b5f5ff047913ab48e7c9
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-06-19 13:44:45 +0300

    Merge with dev

--- server/models/service.py
+++ server/models/service.py
@@ -9,17 +9,12 @@ class Service(db.Model, SerializerMixin):
     name = db.Column(db.String(100), nullable=False, unique=True)
     description = db.Column(db.Text, nullable=True)
     price = db.Column(db.Numeric(10, 2), nullable=False)
-<<<<<<< HEAD
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
-    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
-=======
     duration = db.Column(db.Integer, nullable=False)  # Duration in minutes
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
     
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
    
->>>>>>> b327d6df4718a18179239c3d10a5afbf5207fb7d
 
     # Relationships
     user = db.relationship("User", back_populates="services")
