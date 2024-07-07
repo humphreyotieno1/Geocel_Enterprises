2481500758ee8b6f9c8fd1e4699c09387892b51b
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-07-07 16:39:20 +0300

    Added a footer to the webpage, styled the contact page, fetched services

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,13 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-    darkMode: 'class', // Add this line
-    content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
+  darkMode: 'class', // Enable dark mode if needed
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Specify the files for Tailwind to scan for classes
+  ],
+  theme: {
+    extend: {}, // Extend the default Tailwind theme here if needed
+  },
+  plugins: [
+    require('@tailwindcss/forms'), // Enable Tailwind forms plugin for form styling
+  ],
+};
