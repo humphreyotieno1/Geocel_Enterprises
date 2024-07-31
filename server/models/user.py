f624c0850cf8049f4fcdee5d32cfd1ecd1ce496c
Nyakio-Eva <evalkariuki@gmail.com>
2024-07-31 21:11:24 +0300

    style forgot password email message

--- server/models/user.py
+++ server/models/user.py
@@ -22,7 +22,7 @@ class User(db.Model, SerializerMixin):
     services = db.relationship('Service', back_populates='user', cascade='all, delete-orphan')
 
     
-    #serialization
+   
     
     def __repr__(self):
         return f"<username: {self.user_name}>"
