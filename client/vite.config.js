1f75cc6b3c2d8b2f3f410f18939cdc999c87df69
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-08-08 12:08:50 +0300

    Configured backend resource

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: parseInt(process.env.PORT) || 5173,  // Use the port provided by the environment or default to 5173
+    host: '0.0.0.0',  // Ensure the server listens on all available network interfaces
+  },
+});
